105619cd1d0a6f07f60ef6308d9cf11c66039422
Sarib Siddiqui <110452117+Sarib-Siddique@users.noreply.github.com>
2023-08-10 00:43:26 +0500

    Add files via upload

---
+++ Table/script.js
@@ -0,0 +1,6 @@
+var userinput = +prompt("Enter Table Value");
+var userinput2 = +prompt("Enter Table Value");
+
+for (var i = 1; i <= userinput2; i++) {
+  document.write(userinput + " x " + i + " = " + userinput * i, "<br>");
+}
